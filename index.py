50b0c0a6eebc6684daec5658d6598c99bbd383c2
Kim Clarence Penaflor <kimc.penaflor@gmail.com>
2018-11-27 08:06:44 +0800

    initial commit

---
+++ index.py
@@ -0,0 +1,18 @@
+from services import service
+from util import config
+import base64
+
+
+
+
+# Main API Execution Point
+def lambda_handler(event,context):
+
+    body = event['body']
+
+
+    return 
+
+
+
+
