27a4cf260a248963e432e0bba2386dcfbd922fbf
Kim Clarence Penaflor <kimc.penaflor@gmail.com>
2018-11-28 09:34:48 +0800

    added query filter

--- aws_controllers/dynamodb.py
+++ aws_controllers/dynamodb.py
@@ -16,6 +16,8 @@ def put_item(tableName,item):
         ReturnConsumedCapacity = 'TOTAL'
     )
 
+    return response
+
 # Update Table Item
 def update_item(tableName,key,item):
     global DYNAMODB
@@ -26,11 +28,92 @@ def update_item(tableName,key,item):
         AttributeUpdates = item
     )
 
+    return response
+
+# Increment Item Attribute Count
+def increment_item_value(tableName,key,attribNum):
+    global DYNAMODB
+
+    response = DYNAMODB.update_item(
+        TableName = tableName,
+        Key = key,
+        UpdateExpression = 'ADD #'+attribNum+' :inc',
+        ExpressionAttributeValues = {
+            ':inc' : {
+                'N' : '1'
+            }
+        },
+        ExpressionAttributeNames = {
+            '#'+attribNum : attribNum
+        },
+        ReturnValues = 'UPDATE_NEW'
+    )
+
+    return response
+
+# Update item value
+def update_item(tableName,key,item):
+    global DYNAMODB
+
+    response = DYNAMODB.update_item(
+        TableName = tableName,
+        Key = key,
+        AttributeUpdates = item
+    )
+
+    return response
+
+# Get item by primary key
+def get_item(tableName,key):
+    global DYNAMODB
+
+    response = DYNAMODB.get_item(
+        TableName = tableName,
+        Key = key
+    )
+
+    return response
+
+# Delete Dynamodb Item
+def delete_item(tableName,key):
+    global DYNAMODB
+
+    response = DYNAMODB.delete_item(
+        TableName = tableName,
+        Key = key
+    )
+
+    return response 
+
+# Query db item
+def query(tableName,indexName,filterExp,expAttrbNames,expAttrbValues,limit,exclusiveStartKey=None):
+    global DYNAMO
+
+    if(exclusiveStartKey):
+        response = DYNAMO.query(
+            TableName = tableName,
+            IndexName = indexName,
+            Limit = limit,
+            ExclusiveStartKey = exclusiveStartKey,
+            Select = 'ALL_ATTRIBUTES',
+            KeyConditionExpression = filterExp,
+            ExpressionAttributeNames = expAttrbNames,
+            ExpressionAttributeValues = expAttrbValues
+        )
+    else:
+        response = DYNAMO.query(
+            TableName = tableName,
+            IndexName = indexName,
+            Limit = limit,
+            Select = 'ALL_ATTRIBUTES',
+            KeyConditionExpression = filterExp,
+            ExpressionAttributeNames = expAttrbNames,
+            ExpressionAttributeValues = expAttrbValues
+        )
+
+    return response
 
 
-
-
-
-
+    
 
 
