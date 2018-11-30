39d0449069b0c2910ec90d1bb5f3cd929abbb01f
Kim Clarence Penaflor <kimc.penaflor@gmail.com>
2018-11-30 12:14:31 +0800

    fixed setup.py

--- setup.py
+++ setup.py
@@ -153,7 +153,7 @@ if __name__ == "__main__":
             file_name = 'zf.zip'
             cli_zip_cmd = 'zip -r '+file_name+' * -x .git/ .vscode/ @'
             cli_aws_cmd = 'aws lambda update-function-code --function-name '+function_name
-            os.system(cli_cmd)
+            os.system(cli_zip_cmd)
             os.system(cli_aws_cmd)           
 
 
