c40db42eb1f1a3bf8ac6548fd1ac19c64b4fdcbd
Kim Clarence Penaflor <kimc.penaflor@gmail.com>
2018-11-30 12:17:06 +0800

    added file binary to awspush

--- setup.py
+++ setup.py
@@ -152,7 +152,7 @@ if __name__ == "__main__":
             function_name = settings._value('function_name')
             file_name = 'zf.zip'
             cli_zip_cmd = 'zip -r '+file_name+' * -x .git/ .vscode/ @'
-            cli_aws_cmd = 'aws lambda update-function-code --function-name '+function_name
+            cli_aws_cmd = 'aws lambda update-function-code --function-name '+function_name+' --zip-file fileb://zf.zip'
             os.system(cli_zip_cmd)
             os.system(cli_aws_cmd)           
 
